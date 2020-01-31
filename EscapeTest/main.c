bcbf86bc809030b4373edb132524eaa1ba1abc42
Luna1031 <60143547+Luna1031@users.noreply.github.com>
2020-01-31 12:19:13 +0900

    지수 형태의 실수 상수

--- EscapeTest/main.c
+++ EscapeTest/main.c
@@ -16,6 +16,11 @@ int main()
 	printf("%d과 %d의 합은 %d입니다. \n", 10, 20, 10 + 20);
 	printf("%.1lf-%.1lf=%.1lf \n", 3.4, 1.2, 3.4 - 1.2);
 
+	printf("%.10lf \n", 1e4);
+	printf("%.10lf \n", 3.14e-4);
+	printf("%le \n", 0.00000314);
+	printf("%.2le \n", 0.00000314);
+
 	system("pause");
 	return 0;
 }
