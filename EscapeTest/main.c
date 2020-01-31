e6f06cf97f2184e408d1c291881ad6d0582c38aa
Luna1031 <60143547+Luna1031@users.noreply.github.com>
2020-01-31 11:36:23 +0900

    2장 완료

---
+++ EscapeTest/main.c
@@ -0,0 +1,21 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif //_MSC_VER
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	//type here
+	printf("%d \n", 10);
+	printf("%lf \n", 3.4);
+	printf("%.1lf \n", 3.45);
+	printf("%.10lf \n", 3.4);
+
+	printf("%d과 %d의 합은 %d입니다. \n", 10, 20, 10 + 20);
+	printf("%.1lf-%.1lf=%.1lf \n", 3.4, 1.2, 3.4 - 1.2);
+
+	system("pause");
+	return 0;
+}
