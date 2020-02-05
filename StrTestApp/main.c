a380c55a97ac8b5e74f406b32afda4b46a11ee21
Luna1031 <60143547+Luna1031@users.noreply.github.com>
2020-02-05 13:52:12 +0900

    12

--- StrTestApp/main.c
+++ StrTestApp/main.c
@@ -3,10 +3,9 @@
   version - 1.0
   description - 기본 메인 함수
   --------------------------------------------------------------------------------
-  first created - 2020.02.01
-  writer - Hugo MG Sung.
-*/
-
+  first created - 2020.02.05
+  writer - Ye Eun Lee
+  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
